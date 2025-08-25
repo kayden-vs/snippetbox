842baf04d4be54192a5dc7de71f0cb93f5d46e2a
kayden-vs <rohitr9832@gmail.com>
2025-08-25 18:12:08 +0530

    models for all endpoints

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 
-	"github.com/kayden-vs/snippetbox/ui/html/pages"
+	"github.com/kayden-vs/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -15,13 +15,22 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	component := pages.HomePage()
-	err := component.Render(context.Background(), w)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.errorLog.Println(err.Error())
 		app.serverError(w, err)
 		return
 	}
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
+	// component := pages.HomePage()
+	// err := component.Render(context.Background(), w)
+	// if err != nil {
+	// 	app.errorLog.Println(err.Error())
+	// 	app.serverError(w, err)
+	// 	return
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -31,10 +40,31 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", snippet)
+	// fmt.Fprintf(w, "ID: %d\nTitle: %s\nContent: %s\nCreated: %v\nExpires: %v\n", snippet.ID, snippet.Title, snippet.Content, snippet.Created, snippet.Expires)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	//will be taking this from user later
+	title := "0 Snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-	w.Write([]byte("Create a new snippet..."))
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.infoLog.Println("New Data added, Id: ", id)
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
